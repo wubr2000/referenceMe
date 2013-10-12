d2af45eeb095d0877fa5cfbe053fcc83784719b4
Bruno Wu <wubr2000@bruno-wus-macbook-pro.local>
2013-10-12 22:04:09 +0800

    almost done

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,15 @@
 'use strict';
 
 angular.module('referenceMeApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $rootScope) {
+    
+  	$scope.tags = ["Javascript","HTML","CSS","Funny"];
+  	$scope.clicklink = function (tag) {
+  		$rootScope.category = tag;
+  	};
+
+    //$scope.hipchatLinks = hipchatLinks;
+
+    //console.log($scope.hipchatLinks[1].url);
+    //console.log($scope.hipchatLinks[1]);
   });
